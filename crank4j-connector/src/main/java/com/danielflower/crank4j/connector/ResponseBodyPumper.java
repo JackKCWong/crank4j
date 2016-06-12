0dfd2971cce7f224c5199ab97d3f14a867d31ddb
Daniel Flower <git@danielflower.com>
2016-06-12 18:25:05 +0800

    Bytes from target response to router socket are now synchronous, as I found that the socket was closing before everything was written

--- crank4j-connector/src/main/java/com/danielflower/crank4j/connector/ResponseBodyPumper.java
+++ crank4j-connector/src/main/java/com/danielflower/crank4j/connector/ResponseBodyPumper.java
@@ -3,7 +3,6 @@ package com.danielflower.crank4j.connector;
 import org.eclipse.jetty.client.api.Response;
 import org.eclipse.jetty.util.Callback;
 import org.eclipse.jetty.websocket.api.Session;
-import org.eclipse.jetty.websocket.api.WriteCallback;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -32,31 +31,20 @@ class ResponseBodyPumper implements Response.AsyncContentListener {
         int position = 0;
 
         while (responseBytes.hasRemaining()) {
-
             int size = responseBytes.capacity() - position;
             responseBytes.limit(Math.min(MAX_MESSAGE_SIZE_IN_BYTES, responseBytes.capacity()));
-
             log.debug("Sending " + responseBytes.limit() + " bytes to router");
-            session.getRemote().sendBytes(responseBytes, new WriteCallback() {
-                @Override
-                public void writeFailed(Throwable throwable) {
-                    log.info("Failed to send response data", throwable);
-                    callback.failed(throwable);
-                }
-
-                @Override
-                public void writeSuccess() {
-                    callback.succeeded();
-                }
-            });
+        try {
+                session.getRemote().sendBytes(responseBytes);
+        } catch (IOException e) {
+                log.warn("Error while sending bytes to router", e);
+                callback.failed(e);
+                return;
+        }
             position += size;
             responseBytes.position(Math.min(position, responseBytes.limit()));
-            // TODO should the next loop happen only when the callback has completed? probably...?
         }
-        try {
-            session.getRemote().flush();
-        } catch (IOException e) {
-            log.warn("Error while flushing target response data to router", e);
-        }
+        log.debug("Writing content completed");
+        callback.succeeded();
     }
 }
