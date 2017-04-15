bc5afd85f4a527a3b931d0b911eb048e0704decf
Daniel Flower <git@danielflower.com>
2017-04-15 22:51:09 +0800

    Updated sample properties in the local dir so it can be started locally

--- local/start-router.bat
+++ local/start-router.bat
@@ -1,2 +1,2 @@
 @echo off
-%JDK_18_X64%/bin/java -Dlogback.configurationFile=logback.xml -Dapp.version=1.0-SNAPSHOT -jar ..\crank4j-router\target\crank4j-router-1.0-SNAPSHOT.jar router.properties
+java -Dlogback.configurationFile=logback.xml -Dapp.version=1.0-SNAPSHOT -jar ..\crank4j-router\target\crank4j-router-1.0-SNAPSHOT.jar router.properties
